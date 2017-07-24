bf3abe13190b1353be2c44cc5f2e9dda3fbf9dba
wuzhentao <wuzhentao@lianjia.com>
2017-07-24 21:19:13 +0800

    threeSum &&maximumProduct

---
+++ wuzhentao/maximumProduct.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func maximumProduct(_ nums: [Int]) -> Int {
+        let sorts = nums.sorted()
+        let count = nums.count
+        let sum1 = sorts[0] * sorts[1] * sorts[count - 1]
+        let sum2 = sorts[count - 1] * sorts[count - 2] * sorts[count - 3]
+        return max(sum1, sum2)
+        
+    }
+}
+//Given an integer array, find three numbers whose product is maximum and output the maximum product.
