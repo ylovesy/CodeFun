c231300b8ce44e82986bc781d9acd6563c917b09
songzhanao <songzhanao@lianjia.com>
2017-07-26 00:04:37 +0800

    sza' s Letter Combinations of a Phone Number

---
+++ songzhanao/LetterCombinations.c
@@ -0,0 +1,44 @@
+/**
+ * Return an array of size *returnSize.
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+char **letterCombinations(char *digits, int *returnSize) {
+    char map[10][5] = { " ", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    char **res, *tmp;
+    int num = 1, length = 0, top = 0;
+    while (digits[length]) {
+        if (digits[length] == '0' || digits[length] == '1')
+            continue;
+        else if (digits[length] == '7' || digits[length] == '9')
+            num *= 4;
+        else
+            num *= 3;
+        length++;
+    }
+    res = (char **)malloc(sizeof(char *) * num);
+    if (length == 0) {
+        *returnSize = 0;
+        return res;
+    }
+    tmp = (char *)malloc(sizeof(char) * length);
+    getLetterCom(res, digits, tmp, 0, map, &top);
+    *returnSize = top;
+    return res;
+}
+void getLetterCom(char **res, char *digits, char *tmp, int index, char map[10][5], int *top) {
+    int i, digit = digits[index] - '0';
+    char *letters;
+    if (digits[index] == 0) {
+        letters = (char *)malloc(sizeof(char) * index);
+        tmp[index] = 0;
+        strcpy(letters, tmp);
+        res[*top] = letters;
+        (*top)++;
+        return;
+    }
+    for (i = 0; map[digit][i]; i++) {
+        tmp[index] = map[digit][i];
+        getLetterCom(res, digits, tmp, index + 1, map, top);
+    }
+}
