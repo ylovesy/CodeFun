40f6517c817143f69c4a5164d759a19600b0d9cd
songzhanao <songzhanao@lianjia.com>
2017-08-08 22:42:39 +0800

    coinChange

---
+++ songzhanao/coinChange.java
@@ -0,0 +1,18 @@
+
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+            int dp[] = new int[amount + 1];
+        final int INF = 0x7ffffff;
+        for(int i = 1;i<= amount;i++){
+            dp[i] = INF;
+        }
+        for (int i = 0;i<=amount;i++){
+            for(int j = 0;j<coins.length;j++){
+                if(coins[j]!=INF && i+coins[j]<=amount&&dp[i]!=INF){
+                    dp[i+coins[j]]= Math.min(dp[i+coins[j]],dp[i]+1);
+                }
+            }
+        }
+             return dp[amount] == INF ? -1 : dp[amount];   
+    }
+}
