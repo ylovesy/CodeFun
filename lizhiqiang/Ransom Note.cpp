e7f74d70d6f9f9f8d13ba2503e40a66502070dd4
BlueMercury <395554934@qq.com>
2017-08-10 10:41:49 +0800

    Update and rename 超傻做法Ransom Note.cpp to Ransom Note.cpp

--- lizhiqiang/Ransom Note.cpp
+++ lizhiqiang/Ransom Note.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        int *magList[128] = {0};
+        for (int i = 0; i < magazine.size(); i++) {
+            int charInt = magazine[i];
+            magList[charInt]++;
+        }
+        for (int i = 0; i < ransomNote.size(); i++) {
+            int charInt = ransomNote[i];
+            if(magList[charInt] <= 0) {
+                return false;
+            } else {
+                magList[charInt]--;
+            }
+        }
+        return true;
+    }
+};
+
+//以下做法很傻
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
         sort(ransomNote.begin(), ransomNote.end());
         sort(magazine.begin(),magazine.end());
         int j = 0;
