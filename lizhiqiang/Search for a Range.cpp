ff51b5a2d25b99eab36a3f0552bd74fc49540e7d
BlueMercury <395554934@qq.com>
2017-08-17 00:15:53 +0800

    Update Search for a Range.cpp

--- lizhiqiang/Search for a Range.cpp
+++ lizhiqiang/Search for a Range.cpp
@@ -1,48 +1,23 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int front = 0;
-        int end = nums.size() - 1;
-        int num1 = INT_MIN;
-        int num2 = INT_MAX;
-        vector<int> result;
-        
-        int mid = nums.size() / 2 - 1;
-        while(nums[mid] != target) {
-            if (nums[mid] < target) {
-                front = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+        int l = 0, r = (int)nums.size()-1;
+        vector<int> range{-1, -1};
+        if (nums.empty()) return range; 
+
+        while (l < r) {
+            int m = l + (r - l)/2;
+            nums[m] < target ? l = m+1:r=m;
         }
-        
-        num1 = searchInRange(nums, target, front, mid, false, -1);
-        num2 = searchInRange(nums, target, mid, end, true, -1);
-        if (num1 >= 0 && num2 >= 0) {
-            result.push_back(num1);
-            result.push_back(num2);
+        if (nums[l] != target) return range;
+        range[0] = l;
+        r = (int) nums.size()-1;
+        while (l < r) {
+            int m = l + (r - l)/2+1;
+            nums[m] > target ? r=m-1:l=m;
         }
-        
-        
-        return result;
-    }
+        range[1] = r;
+        return range;
+   }
     
-    int searchInRange(vector<int>& nums, int target, int start, int end, bool isSearchMax, int currentPos) {
-        int mid = start + ((end - start) / 2);
-        if (start < end) {
-        if (nums[mid] == target) {
-            if (isSearchMax) {
-                return searchInRange(nums, target, mid + 1, end, isSearchMax, mid);
-            } else {
-                return searchInRange(nums, target, start, mid - 1, isSearchMax, mid);
-            }
-        } else if (nums[mid] < target) {
-            return searchInRange(nums, target, mid + 1, end, isSearchMax, currentPos);
-        } else {
-            return searchInRange(nums, target, start, mid - 1, isSearchMax, currentPos);
-        }
-        } else {
-            return currentPos;
-        }
-    }
 };
