2982d470e7108823473d57bd7c3a1814a797a83c
handa <handa@handadeMacintosh.local>
2017-08-21 23:16:56 +0800

    191. Number of 1 Bits

--- handa/Solution.swift
+++ handa/Solution.swift
@@ -917,5 +917,15 @@ class Solution: NSObject {
         }
         return arr[m][n]
     }
+    //MARK: - 191. Number of 1 Bits
+    func hammingWeight(_ n: Int) -> Int{
+        var count = 0
+        var n = n
+        while (n > 0) {
+            count += 1
+            n = n & (n - 1)
+        }
+        return count;
+    }
     
 }
