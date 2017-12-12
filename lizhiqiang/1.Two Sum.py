4fff85c87361e352cacaa90d3c50ba73adf89d15
BlueMercury <395554934@qq.com>
2017-12-12 20:06:15 +0800

    Update 1.Two Sum.py

--- lizhiqiang/1.Two Sum.py
+++ lizhiqiang/1.Two Sum.py
@@ -14,3 +14,30 @@ class Solution(object):
                 end = end - 1;
             else :
                 return [front, end]
+            
+            
+class Solution(object):
+    def twoSum(self, nums, target):
+        sortedNums = sorted(nums)
+        i=0
+        j=len(nums)-1
+        while(j > i):
+            if sortedNums[i] + sortedNums[j] > target:
+                j = j - 1
+            elif sortedNums[i] + sortedNums[j] < target:
+                i = i + 1
+            else:
+                break
+        if (i < j) :
+            a = -1
+            b = -1
+            for index , num in enumerate(nums) :
+                if num == sortedNums[i] and a == -1 :
+                    a = index
+                    continue
+                if num == sortedNums[j] and b == -1:
+                    b = index
+                    continue
+            return a,b
+        return None
+        
